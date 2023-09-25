ad6715890beede2bba36deae33cae7758d449966
joshuaz <joshuaz@users.noreply.github.com>
2023-09-25 18:30:27 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas as pd
 
 streamlit.title("My Parents New Healthy Diner")
 streamlit.header("Breakfast Menu")
@@ -7,5 +8,5 @@ streamlit.text("Kale, Spinach, & Rocket Smoothie")
 streamlit.text("Hard Boiled, Free-Range Eggs")
 
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
