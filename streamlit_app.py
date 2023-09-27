5312fd08f72ee71ed2d5894789621e36eba3a6a3
joshuaz <joshuaz@users.noreply.github.com>
2023-09-27 10:25:15 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ streamlit.dataframe(my_fruit_list)
 streamlit.header("Fruityvice Fruit Advice!")
 
 try:
-  
+  streamlit.stop()
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
   streamlit.text(fruityvice_response)
   
